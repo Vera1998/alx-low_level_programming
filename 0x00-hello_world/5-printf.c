a0240ad7534ba3bccf44ba69f8878a70e18193db
Vera1998 <ayitsovera98@gmail.com>
2022-12-29 01:37:05 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,10 @@
 /**
  * main - print a c program using printf
  *
- * Return : 0
+ * Return: 0
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
